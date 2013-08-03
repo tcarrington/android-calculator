69b19c4b20a33bcca01c084e5a27069620131308
Taylor Carrington <taylor.carrington91@gmail.com>
2013-08-03 15:29:37 -0700

    fixed formating for first row buttons

--- src/com/example/calculator/MainActivity.java
+++ src/com/example/calculator/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.calculator;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		
+		int width = metrics.widthPixels;
+		Button d_button = (Button) findViewById(R.id.d_button);
+		Button e_button = (Button) findViewById(R.id.e_button);
+		Button f_button = (Button) findViewById(R.id.f_button);
+		Button div_button = (Button) findViewById(R.id.div_button);
+		d_button.setWidth((width/4)-8);
+		e_button.setWidth((width/4)-8);
+		f_button.setWidth((width/4)-8);
+		div_button.setWidth((width/4)-8);
+		
+
 	}
 
 	@Override
