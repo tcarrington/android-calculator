ba5af4193281c91a6b4c3f995031e2c3b4523aee
Taylor Carrington <taylor.carrington91@gmail.com>
2013-08-04 19:02:09 -0700

    removed radio buttons, added new buttons with highlight feature

--- gen/com/example/calculator/R.java
+++ gen/com/example/calculator/R.java
@@ -21,41 +21,41 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int a_button=0x7f09000b;
-        public static final int action_settings=0x7f09001f;
-        public static final int add_button=0x7f09001a;
-        public static final int b_button=0x7f09000c;
-        public static final int bin_radio=0x7f090003;
-        public static final int button_0=0x7f09001b;
-        public static final int button_1=0x7f090017;
-        public static final int button_2=0x7f090018;
-        public static final int button_3=0x7f090019;
-        public static final int button_4=0x7f090013;
-        public static final int button_5=0x7f090014;
-        public static final int button_6=0x7f090015;
-        public static final int button_7=0x7f09000f;
-        public static final int button_8=0x7f090010;
-        public static final int button_9=0x7f090011;
-        public static final int c_button=0x7f09000d;
-        public static final int clear_button=0x7f09000a;
-        public static final int d_button=0x7f090007;
-        public static final int dec_radio=0x7f090005;
+        public static final int a_button=0x7f09000a;
+        public static final int action_settings=0x7f09001e;
+        public static final int add_button=0x7f090019;
+        public static final int b_button=0x7f09000b;
+        public static final int bin_select=0x7f090002;
+        public static final int button_0=0x7f09001a;
+        public static final int button_1=0x7f090016;
+        public static final int button_2=0x7f090017;
+        public static final int button_3=0x7f090018;
+        public static final int button_4=0x7f090012;
+        public static final int button_5=0x7f090013;
+        public static final int button_6=0x7f090014;
+        public static final int button_7=0x7f09000e;
+        public static final int button_8=0x7f09000f;
+        public static final int button_9=0x7f090010;
+        public static final int c_button=0x7f09000c;
+        public static final int clear_button=0x7f090009;
+        public static final int d_button=0x7f090006;
+        public static final int dec_select=0x7f090004;
         public static final int display_main=0x7f090001;
         public static final int display_secondary=0x7f090000;
-        public static final int div_button=0x7f09000e;
-        public static final int e_button=0x7f090008;
-        public static final int equal_button=0x7f09001d;
-        public static final int f_button=0x7f090009;
-        public static final int hex_radio=0x7f090006;
-        public static final int mod_button=0x7f09001e;
-        public static final int mult_button=0x7f090012;
-        public static final int oct_radio=0x7f090004;
-        public static final int plusminus_button=0x7f09001c;
-        public static final int radiogroup=0x7f090002;
-        public static final int sub_button=0x7f090016;
+        public static final int div_button=0x7f09000d;
+        public static final int e_button=0x7f090007;
+        public static final int equal_button=0x7f09001c;
+        public static final int f_button=0x7f090008;
+        public static final int hex_select=0x7f090005;
+        public static final int mod_button=0x7f09001d;
+        public static final int mult_button=0x7f090011;
+        public static final int oct_select=0x7f090003;
+        public static final int plusminus_button=0x7f09001b;
+        public static final int sub_button=0x7f090015;
     }
     public static final class integer {
         public static final int button1row=0x7f050000;
@@ -72,7 +72,7 @@ public final class R {
         public static final int add_button=0x7f06001b;
         public static final int app_name=0x7f060000;
         public static final int b_button=0x7f06000d;
-        public static final int bin_radio=0x7f060004;
+        public static final int bin_select=0x7f060004;
         public static final int button_0=0x7f06001c;
         public static final int button_1=0x7f060018;
         public static final int button_2=0x7f060019;
@@ -86,30 +86,22 @@ public final class R {
         public static final int c_button=0x7f06000c;
         public static final int clear_button=0x7f06000b;
         public static final int d_button=0x7f060008;
-        public static final int dec_radio=0x7f060006;
+        public static final int dec_select=0x7f060006;
         public static final int display_main=0x7f060002;
         public static final int display_secondary=0x7f060003;
         public static final int div_button=0x7f06000f;
         public static final int e_button=0x7f060009;
         public static final int equal_button=0x7f06001e;
         public static final int f_button=0x7f06000a;
-        public static final int hex_radio=0x7f060007;
+        public static final int hex_select=0x7f060007;
         public static final int mod_button=0x7f06001f;
         public static final int mult_button=0x7f060013;
-        public static final int oct_radio=0x7f060005;
+        public static final int oct_select=0x7f060005;
         public static final int plusminus_button=0x7f06001d;
         public static final int sub_button=0x7f060017;
     }
     public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
+        /**  theme for current API and past APIs 
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
@@ -123,9 +115,6 @@ public final class R {
  API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f070000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
         public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/calculator/MainActivity.java
+++ src/com/example/calculator/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.calculator;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -33,10 +34,10 @@ public class MainActivity extends Activity {
 		int height = metrics.heightPixels;
 		TextView display_main = (TextView) findViewById(R.id.display_main);
 		TextView display_secondary = (TextView) findViewById(R.id.display_secondary);
-		RadioButton bin_radio = (RadioButton) findViewById(R.id.bin_radio);
-		RadioButton oct_radio = (RadioButton) findViewById(R.id.oct_radio);
-		RadioButton dec_radio = (RadioButton) findViewById(R.id.dec_radio);
-		RadioButton hex_radio = (RadioButton) findViewById(R.id.hex_radio);
+		Button bin_select = (Button) findViewById(R.id.bin_select);
+		Button oct_select = (Button) findViewById(R.id.oct_select);
+		Button dec_select = (Button) findViewById(R.id.dec_select);
+		Button hex_select = (Button) findViewById(R.id.hex_select);
 		Button d_button = (Button) findViewById(R.id.d_button);
 		Button e_button = (Button) findViewById(R.id.e_button);
 		Button f_button = (Button) findViewById(R.id.f_button);
@@ -64,10 +65,10 @@ public class MainActivity extends Activity {
 		//width formatting
 		display_secondary.setWidth(width-16);
 		display_main.setWidth(width-16);
-		bin_radio.setWidth((width/4)-16);
-		oct_radio.setWidth((width/4)-16);
-		dec_radio.setWidth((width/4)-16);
-		hex_radio.setWidth((width/4)-16);
+		bin_select.setWidth((width/4)-16);
+		oct_select.setWidth((width/4)-16);
+		dec_select.setWidth((width/4)-16);
+		hex_select.setWidth((width/4)-16);
 		d_button.setWidth((width/4)-16);
 		e_button.setWidth((width/4)-16);
 		f_button.setWidth((width/4)-16);
@@ -96,10 +97,10 @@ public class MainActivity extends Activity {
 		//height formatting
 		display_secondary.setHeight(((height)/9)-16);
 		display_main.setHeight(((height)/9)-16);
-		bin_radio.setHeight((height/9)-16);
-		oct_radio.setHeight((height/9)-16);
-		dec_radio.setHeight((height/9)-16);
-		hex_radio.setHeight((height/9)-16);
+		bin_select.setHeight((height/9)-16);
+		oct_select.setHeight((height/9)-16);
+		dec_select.setHeight((height/9)-16);
+		hex_select.setHeight((height/9)-16);
 		d_button.setHeight((height/9)-16);
 		e_button.setHeight((height/9)-16);
 		f_button.setHeight((height/9)-16);
@@ -126,10 +127,7 @@ public class MainActivity extends Activity {
 		equal_button.setHeight((height/9)-16);
 		mod_button.setHeight((height/9)-16);
 		//set defaults for radio buttons
-		bin_radio.setChecked(true);
-		oct_radio.setChecked(false);
-		dec_radio.setChecked(false);
-		hex_radio.setChecked(false);
+
 		//required assignment for leading zero at onCreate()
 		displayValue = "0";
 
@@ -142,11 +140,7 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-
-	public void onRadioButtonClicked(View view) {
-	    // Is the button now checked?
-	    boolean checked = ((RadioButton) view).isChecked();
-	    
+	public void bin_select(View view){
 		Button d_button = (Button) findViewById(R.id.d_button);
 		Button e_button = (Button) findViewById(R.id.e_button);
 		Button f_button = (Button) findViewById(R.id.f_button);
@@ -163,93 +157,176 @@ public class MainActivity extends Activity {
 		Button button_2 = (Button) findViewById(R.id.button_2);
 		Button button_3 = (Button) findViewById(R.id.button_3);
 		Button button_0 = (Button) findViewById(R.id.button_0);
-		
-	    // Check which radio button was clicked
-	    switch(view.getId()) {
-	        case R.id.bin_radio:
-	            if (checked)
-	                // TODO: add logic for binary calculations and conversions
-	            	f_button.setEnabled(false);
-	            	e_button.setEnabled(false);
-	            	d_button.setEnabled(false);
-	            	c_button.setEnabled(false);
-	            	b_button.setEnabled(false);
-	            	a_button.setEnabled(false);
-	            	button_9.setEnabled(false);
-	            	button_8.setEnabled(false);
-	            	button_7.setEnabled(false);
-	            	button_6.setEnabled(false);
-	            	button_5.setEnabled(false);
-	            	button_4.setEnabled(false);
-	            	button_3.setEnabled(false);
-	            	button_2.setEnabled(false);
-	            	button_1.setEnabled(true);
-	            	button_0.setEnabled(true);
-	            break;
-	        case R.id.oct_radio:
-	            if (checked)
-	                // TODO: add logic for octal calculations and conversions
-	            	f_button.setEnabled(false);
-	            	e_button.setEnabled(false);
-	            	d_button.setEnabled(false);
-	            	c_button.setEnabled(false);
-	            	b_button.setEnabled(false);
-	            	a_button.setEnabled(false);
-	            	button_9.setEnabled(false);
-	            	button_8.setEnabled(false);
-	            	button_7.setEnabled(true);
-	            	button_6.setEnabled(true);
-	            	button_5.setEnabled(true);
-	            	button_4.setEnabled(true);
-	            	button_3.setEnabled(true);
-	            	button_2.setEnabled(true);
-	            	button_1.setEnabled(true);
-	            	button_0.setEnabled(true);
-	            break;
-	        case R.id.dec_radio:
-	        	if (checked)
-	        		// TODO: add logic for decimal calculations and conversions
-	            	f_button.setEnabled(false);
-	            	e_button.setEnabled(false);
-	            	d_button.setEnabled(false);
-	            	c_button.setEnabled(false);
-	            	b_button.setEnabled(false);
-	            	a_button.setEnabled(false);
-	            	button_9.setEnabled(true);
-	            	button_8.setEnabled(true);
-	            	button_7.setEnabled(true);
-	            	button_6.setEnabled(true);
-	            	button_5.setEnabled(true);
-	            	button_4.setEnabled(true);
-	            	button_3.setEnabled(true);
-	            	button_2.setEnabled(true);
-	            	button_1.setEnabled(true);
-	            	button_0.setEnabled(true);
-	        	break;
-	        case R.id.hex_radio:
-	        	if(checked)
-	        		// TODO: add logic for hexadecimal calculations and conversions
-	            	f_button.setEnabled(true);
-	            	e_button.setEnabled(true);
-	            	d_button.setEnabled(true);
-	            	c_button.setEnabled(true);
-	            	b_button.setEnabled(true);
-	            	a_button.setEnabled(true);
-	            	button_9.setEnabled(true);
-	            	button_8.setEnabled(true);
-	            	button_7.setEnabled(true);
-	            	button_6.setEnabled(true);
-	            	button_5.setEnabled(true);
-	            	button_4.setEnabled(true);
-	            	button_3.setEnabled(true);
-	            	button_2.setEnabled(true);
-	            	button_1.setEnabled(true);
-	            	button_0.setEnabled(true);
+    	f_button.setEnabled(false);
+    	e_button.setEnabled(false);
+    	d_button.setEnabled(false);
+    	c_button.setEnabled(false);
+    	b_button.setEnabled(false);
+    	a_button.setEnabled(false);
+    	button_9.setEnabled(false);
+    	button_8.setEnabled(false);
+    	button_7.setEnabled(false);
+    	button_6.setEnabled(false);
+    	button_5.setEnabled(false);
+    	button_4.setEnabled(false);
+    	button_3.setEnabled(false);
+    	button_2.setEnabled(false);
+    	button_1.setEnabled(true);
+    	button_0.setEnabled(true);
+    	
+    	
+    	Resources res = getResources();
+    	Drawable shape = res.getDrawable(R.drawable.button_background);
 
-	        	break;
-	    }
+    	Button bin_select = (Button) findViewById(R.id.bin_select);
+    	Button oct_select = (Button) findViewById(R.id.oct_select);
+    	Button dec_select = (Button) findViewById(R.id.dec_select);
+    	Button hex_select = (Button) findViewById(R.id.hex_select);
+    	bin_select.setBackground(shape);
+    	oct_select.setBackground(null);
+    	dec_select.setBackground(null);
+    	hex_select.setBackground(null);
+	}
+	public void oct_select(View view){
+		Button d_button = (Button) findViewById(R.id.d_button);
+		Button e_button = (Button) findViewById(R.id.e_button);
+		Button f_button = (Button) findViewById(R.id.f_button);
+		Button a_button = (Button) findViewById(R.id.a_button);
+		Button b_button = (Button) findViewById(R.id.b_button);
+		Button c_button = (Button) findViewById(R.id.c_button);
+		Button button_7 = (Button) findViewById(R.id.button_7);
+		Button button_8 = (Button) findViewById(R.id.button_8);
+		Button button_9 = (Button) findViewById(R.id.button_9);
+		Button button_4 = (Button) findViewById(R.id.button_4);
+		Button button_5 = (Button) findViewById(R.id.button_5);
+		Button button_6 = (Button) findViewById(R.id.button_6);
+		Button button_1 = (Button) findViewById(R.id.button_1);
+		Button button_2 = (Button) findViewById(R.id.button_2);
+		Button button_3 = (Button) findViewById(R.id.button_3);
+		Button button_0 = (Button) findViewById(R.id.button_0);
+    	f_button.setEnabled(false);
+    	e_button.setEnabled(false);
+    	d_button.setEnabled(false);
+    	c_button.setEnabled(false);
+    	b_button.setEnabled(false);
+    	a_button.setEnabled(false);
+    	button_9.setEnabled(false);
+    	button_8.setEnabled(false);
+    	button_7.setEnabled(true);
+    	button_6.setEnabled(true);
+    	button_5.setEnabled(true);
+    	button_4.setEnabled(true);
+    	button_3.setEnabled(true);
+    	button_2.setEnabled(true);
+    	button_1.setEnabled(true);
+    	button_0.setEnabled(true);
+    	
+    	Resources res = getResources();
+    	Drawable shape = res.getDrawable(R.drawable.button_background);
+
+    	Button bin_select = (Button) findViewById(R.id.bin_select);
+    	Button oct_select = (Button) findViewById(R.id.oct_select);
+    	Button dec_select = (Button) findViewById(R.id.dec_select);
+    	Button hex_select = (Button) findViewById(R.id.hex_select);
+    	bin_select.setBackground(null);
+    	oct_select.setBackground(shape);
+    	dec_select.setBackground(null);
+    	hex_select.setBackground(null);
+	}
+	public void dec_select(View view){
+		Button d_button = (Button) findViewById(R.id.d_button);
+		Button e_button = (Button) findViewById(R.id.e_button);
+		Button f_button = (Button) findViewById(R.id.f_button);
+		Button a_button = (Button) findViewById(R.id.a_button);
+		Button b_button = (Button) findViewById(R.id.b_button);
+		Button c_button = (Button) findViewById(R.id.c_button);
+		Button button_7 = (Button) findViewById(R.id.button_7);
+		Button button_8 = (Button) findViewById(R.id.button_8);
+		Button button_9 = (Button) findViewById(R.id.button_9);
+		Button button_4 = (Button) findViewById(R.id.button_4);
+		Button button_5 = (Button) findViewById(R.id.button_5);
+		Button button_6 = (Button) findViewById(R.id.button_6);
+		Button button_1 = (Button) findViewById(R.id.button_1);
+		Button button_2 = (Button) findViewById(R.id.button_2);
+		Button button_3 = (Button) findViewById(R.id.button_3);
+		Button button_0 = (Button) findViewById(R.id.button_0);
+    	f_button.setEnabled(false);
+    	e_button.setEnabled(false);
+    	d_button.setEnabled(false);
+    	c_button.setEnabled(false);
+    	b_button.setEnabled(false);
+    	a_button.setEnabled(false);
+    	button_9.setEnabled(true);
+    	button_8.setEnabled(true);
+    	button_7.setEnabled(true);
+    	button_6.setEnabled(true);
+    	button_5.setEnabled(true);
+    	button_4.setEnabled(true);
+    	button_3.setEnabled(true);
+    	button_2.setEnabled(true);
+    	button_1.setEnabled(true);
+    	button_0.setEnabled(true);
+    	
+    	Resources res = getResources();
+    	Drawable shape = res.getDrawable(R.drawable.button_background);
+
+    	Button bin_select = (Button) findViewById(R.id.bin_select);
+    	Button oct_select = (Button) findViewById(R.id.oct_select);
+    	Button dec_select = (Button) findViewById(R.id.dec_select);
+    	Button hex_select = (Button) findViewById(R.id.hex_select);
+    	bin_select.setBackground(null);
+    	oct_select.setBackground(null);
+    	dec_select.setBackground(shape);
+    	hex_select.setBackground(null);
+	}
+	public void hex_select(View view){
+		Button d_button = (Button) findViewById(R.id.d_button);
+		Button e_button = (Button) findViewById(R.id.e_button);
+		Button f_button = (Button) findViewById(R.id.f_button);
+		Button a_button = (Button) findViewById(R.id.a_button);
+		Button b_button = (Button) findViewById(R.id.b_button);
+		Button c_button = (Button) findViewById(R.id.c_button);
+		Button button_7 = (Button) findViewById(R.id.button_7);
+		Button button_8 = (Button) findViewById(R.id.button_8);
+		Button button_9 = (Button) findViewById(R.id.button_9);
+		Button button_4 = (Button) findViewById(R.id.button_4);
+		Button button_5 = (Button) findViewById(R.id.button_5);
+		Button button_6 = (Button) findViewById(R.id.button_6);
+		Button button_1 = (Button) findViewById(R.id.button_1);
+		Button button_2 = (Button) findViewById(R.id.button_2);
+		Button button_3 = (Button) findViewById(R.id.button_3);
+		Button button_0 = (Button) findViewById(R.id.button_0);
+    	f_button.setEnabled(true);
+    	e_button.setEnabled(true);
+    	d_button.setEnabled(true);
+    	c_button.setEnabled(true);
+    	b_button.setEnabled(true);
+    	a_button.setEnabled(true);
+    	button_9.setEnabled(true);
+    	button_8.setEnabled(true);
+    	button_7.setEnabled(true);
+    	button_6.setEnabled(true);
+    	button_5.setEnabled(true);
+    	button_4.setEnabled(true);
+    	button_3.setEnabled(true);
+    	button_2.setEnabled(true);
+    	button_1.setEnabled(true);
+    	button_0.setEnabled(true);
+    	
+    	Resources res = getResources();
+    	Drawable shape = res.getDrawable(R.drawable.button_background);
+
+    	Button bin_select = (Button) findViewById(R.id.bin_select);
+    	Button oct_select = (Button) findViewById(R.id.oct_select);
+    	Button dec_select = (Button) findViewById(R.id.dec_select);
+    	Button hex_select = (Button) findViewById(R.id.hex_select);
+    	bin_select.setBackground(null);
+    	oct_select.setBackground(null);
+    	dec_select.setBackground(null);
+    	hex_select.setBackground(shape);
 	}
 	
+		
 	//first row of buttons
 	public void send_d(View view) {
 		TextView display_main = (TextView) findViewById(R.id.display_main);
