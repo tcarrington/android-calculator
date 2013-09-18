d35ec18b6736de68509cf9d85e12e9f4434d2e8b
Taylor Carrington <taylor.carrington91@gmail.com>
2013-09-17 21:43:44 -0700

    updated minor documentation

--- src/programming/calculator/MainActivity.java
+++ src/programming/calculator/MainActivity.java
@@ -1,25 +1,15 @@
 /********************************************************************************
-*******************************PROJECT INFORMATION*******************************
+ *******************************PROJECT INFORMATION*******************************
 
-Project:		Programming Calculator
-Author: 		Taylor Carrington
-Created: 		August 3, 2013
-Repository: 	https://github.com/tcarrington/android-calculator
-Description:	Programming Calculator - Do calculations and convert numbers in binary, octal, decimal, hexadecimal.
-Notes:			This is my first Java and Android project.  Lots of mistakes but learning lots! : )
-Goal:			Post on Google Play!
+ Project:		Programming Calculator
+ Author: 		Taylor Carrington
+ Created: 		August 3, 2013
+ Repository: 	https://github.com/tcarrington/android-calculator
+ Description:	Programming Calculator - Do calculations and convert numbers in binary, octal, decimal, hexadecimal.
+ Notes:			This is my first Java and Android project.  Lots of mistakes but learning lots! : )
+ Goal:			Post on Google Play!
 
-//List of builds that have been pushed to play (alpha, beta, production)
-Production 	v1.0: 	September 8, 2013
-Beta    	v1.0.1: September 9, 2013
-Production	v1.1.0: September 13, 2013
-Production  v1.1.1: September 14, 2013
-
-
-//Projected Release Dates
-
-
-********************************************************************************/
+ ********************************************************************************/
 
 package programming.calculator;
 
